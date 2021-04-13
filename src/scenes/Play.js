0c2949f8b4ad84f52f9fa8e8bd42ecadbb09a313
lhan10 <lhan10@ucsc.edu>
2021-04-12 21:52:53 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -65,6 +65,25 @@ class Play extends Phaser.Scene{
             }),
             frameRate: 30
         });
+
+        // initialize score
+        this.p1Score = 0;
+
+        // display score
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize
+        + borderPadding*2, this.p1Score, scoreConfig);
     }
 
     update() {
