7934434eec74e1381f267f5084254803d176c040
lhan10 <lhan10@ucsc.edu>
2021-04-13 00:51:33 -0700

    update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -86,26 +86,35 @@ class Play extends Phaser.Scene{
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize
         + borderPadding*2, this.p1Score, scoreConfig);
         
-
+        // GAME OVER flag
+        this.gameOver = false;
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(10000,() => {
+        this.clock = this.time.delayedCall(60000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER',
             scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 +64,
-            '(F)ire to Restart', scoreConfig).setOrigin(0.5);}, null, this);
+            'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+            }, null, this);
     }
+
     update() {
+
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
+
         this.starfield.tilePositionX -= 4;
 
-        //update rocket
-        this.p1Rocket.update();
-
-        //update spaceship(x3)
-        this.ship01.update();
+        if (!this.gameOver){
+        this.p1Rocket.update();  //update rocket
+        this.ship01.update();  //update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+        }
         
         // check collisions
         if(this.checkCollison(this.p1Rocket, this.ship03)) {
@@ -148,5 +157,7 @@ class Play extends Phaser.Scene{
         // score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+
+        this.sound.play('sfx_explosion');
     }
 }
